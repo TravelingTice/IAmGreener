092bc1ae64aaacaec4674d6ef8733420c7d1f36e
Philippa von Graevemeyer <pvgraevemeyer@gmail.com>
2020-03-03 15:14:30 +0100

    route home

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :users do
     resources :cars, only: [:new, :create]
+    resources :home, only: [:new, :create]
   end
 end
